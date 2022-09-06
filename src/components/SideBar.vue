cd7423a9ccb16bfbe158cd93bb66a0930393f4dc
RAFAEL CARRENHO TARANTO <rcarrenhot@gmail.com>
2022-09-06 20:57:07 -0300

    First Commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sidebar" >
+    <div class="navigation">
+      <div class="char-nav" v-for="char in characters" v-bind:key="char.id">{{char.name}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default({
+  name:"SideBar",
+  data() {
+    return{
+      
+      
+      characters:[
+      {
+        id:'1',
+        name:'Tighnari'
+      },
+      {
+        id:'2',
+        name:'Kujou Sarah'
+      },
+      {
+        id:'3',
+        name:'Kuki Shinobu'
+      },
+      {
+        id:'4',
+        name:'Collei'
+      },
+    ]
+    }
+  },
+
+})
+</script>
