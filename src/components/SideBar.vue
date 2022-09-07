f503bfcdc4cb36126d2a48c789f9c75e5c7a9bb5
RAFAEL CARRENHO TARANTO <rcarrenhot@gmail.com>
2022-09-06 21:40:00 -0300

    Doing navigation

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -32,9 +32,56 @@ export default({
         id:'4',
         name:'Collei'
       },
+      {
+        id:'5',
+        name:'Ayato'
+      },
+      {
+        id:'6',
+        name:'Xiangling'
+      },
+      {
+        id:'7',
+        name:'Kazuha'
+      },
     ]
     }
   },
 
 })
 </script>
+<style>
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+  .sidebar{
+    height:100vh;
+    width:13vw;
+    position: fixed; 
+    z-index: 1;
+    top: 0; 
+    right: 0;
+    background-color: #111;
+    color:#eee;
+    overflow-x: hidden;
+    padding: 10px;
+  }
+  .navigation{
+    width:100%;
+    height:100%;
+    display: block;
+    flex-direction: row;
+    flex-wrap: wrap;
+    overflow-y:scroll;
+  }
+  .char-nav{
+    aspect-ratio: 1/1;
+    width:92%;
+    height:auto;
+    margin:5px;
+    background-color: rgba(255, 255, 255, 0.070);
+  }
+
+</style>
